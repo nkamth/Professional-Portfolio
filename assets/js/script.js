dbf6be02856ac1dc7c5e8977efd0ce69b1427c82
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-07-31 23:21:27 -0400

    Added link to project 2 jokr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-var mainProjectImg=document.querySelector("#Main-Project");
+var mainProjectImg = document.querySelector("#Main-Project");
 
-function openImg(){
-    var source = "https://roo116.github.io/character-search"
-    window.open(source);
+function openImg() {
+  var source = "https://roo116.github.io/character-search";
+  window.open(source);
+}
+
+function openImgJokr() {
+  var source = "https://github.com/dolcebasstrombone/jokr";
+  window.open(source);
 }
